4dedc297d382ff652069dba05efdf2d193c96e0c
chelseardinh <chelseadinh94@gmail.com>
2020-05-08 12:26:39 -0700

    Create app and add NavBar component

---
+++ personal-website/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './components/navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		<NavBar />
+		<div className="Test-div">Hello world.</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ personal-website/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<header>
+					<nav className="navbar">
+						NavBar text placeholder.
+					</nav>
+				</header>
+			</React.Fragment>
+		);
+	}
+}
+
+export default NavBar;
