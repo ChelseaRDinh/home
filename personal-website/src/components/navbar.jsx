7b7718fd02af892e3369212a0071fe07ab809a0a
chelseardinh <chelseadinh94@gmail.com>
2020-05-09 10:54:47 -0700

    Add JS for menu toggle on mobile

--- personal-website/src/components/navbar.jsx
+++ personal-website/src/components/navbar.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class NavBar extends Component {
+	constructor(props) {
+		super(props);
+		this.toggleMenu = this.toggleMenu.bind(this);
+	}
+
+	toggleMenu() {
+		let navbarList = document.getElementById('js-navbar__list');
+		let navbarToggle = document.getElementById('js-navbar-toggle');
+
+		navbarToggle.addEventListener('click', () => {
+			navbarList.classList.toggle('active');
+			console.log('Click happened.');
+		});
+	}
+
 	render() {
 		return (
 			<div className="navbar">
 				<nav>
-					<ul>
-						<li><a class="about" href="#About">About</a></li>
-						<li><a class="projects" href="#Projects">Projects</a></li>
-						<li><a class="contact" href="#Contact">Contact</a></li>
+					<span className="navbar-toggle" id="js-navbar-toggle" onClick={this.toggleMenu}>
+						<FontAwesomeIcon icon={faBars} />
+					</span>
+					<a href='#' className="navbar-logo">logo</a>
+					<ul className="navbar__list" id="js-navbar__list">
+						<li><a className="navbar__links" href="#About">About</a></li>
+						<li><a className="navbar__links" href="#Projects">Projects</a></li>
+						<li><a className="navbar__links" href="#Contact">Contact</a></li>
 					</ul>
 				</nav>
 			</div>
