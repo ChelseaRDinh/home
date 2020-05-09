5ee3b2ed1f0453ae729fad534b6cd71cda5ba75b
chelseardinh <chelseadinh94@gmail.com>
2020-05-09 08:23:02 -0700

    Make navbar sticky

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -12,9 +12,19 @@ import './App.scss';
 function App() {
   return (
     <div className="app">
-		<NavBar />
+		<header>
+			<NavBar />
+		</header>
 		<main>
-			Main content.
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec felis ac velit euismod elementum. Cras tincidunt est non nunc viverra imperdiet a ut erat. Donec vehicula nisi in metus auctor sollicitudin. Cras convallis posuere ante sed ultrices. Nam sed urna magna. Aenean maximus non dui cursus pretium. Aliquam vel accumsan tellus, et sodales lacus. In mi mi, efficitur vitae varius eu, dignissim sit amet massa. Donec pulvinar auctor nunc. In nec fermentum nibh. Quisque ut suscipit quam. Sed volutpat congue venenatis.
+
+			In dictum dui semper cursus sollicitudin. Aliquam consequat, diam et consequat sollicitudin, urna est lobortis ipsum, nec volutpat est nisl in ligula. Duis malesuada est ligula, quis fringilla odio mollis in. Curabitur efficitur, purus vehicula gravida ultricies, neque velit mattis lorem, eleifend varius orci sapien a ex. Cras at vestibulum ligula, sed blandit sem. Praesent scelerisque velit at urna imperdiet viverra. Aenean non fermentum magna. Mauris a placerat dui. Proin finibus aliquam pellentesque.
+
+			Vivamus eleifend consequat rutrum. Vestibulum non blandit elit, vel interdum metus. Nulla vitae viverra turpis. Nam vestibulum quam eget sem porttitor, eget iaculis enim euismod. Ut nisi ligula, sagittis in rutrum sed, blandit ut est. Vestibulum consectetur arcu sed quam commodo, id vulputate ex pharetra. Suspendisse non elementum justo, ac luctus orci. Morbi ante erat, faucibus at ante sed, malesuada tincidunt metus. Vestibulum feugiat vel ante eu finibus. Phasellus nec nunc et nulla placerat semper. Nunc euismod, augue sed convallis pharetra, risus ligula tristique lacus, a sagittis nunc leo a massa. Nam odio tellus, vehicula a turpis quis, tempus finibus enim. Maecenas interdum ligula id felis viverra malesuada.
+
+			Curabitur eget egestas erat, eget venenatis dui. Proin augue metus, porttitor vitae felis iaculis, porta vulputate lectus. Vestibulum porttitor, nulla non euismod tristique, purus nunc rutrum dui, vel facilisis neque odio id ipsum. Nam venenatis lobortis dolor, non ultricies odio lacinia ac. Aliquam erat volutpat. Sed finibus lectus iaculis egestas semper. Etiam sollicitudin consequat elit vitae sollicitudin. Integer mollis magna sed turpis euismod tempus. Nunc sollicitudin et augue sed blandit. Nunc at lobortis mauris. Pellentesque elementum tellus id arcu ornare commodo.
+
+			Phasellus sed bibendum risus, sit amet rutrum dui. Praesent venenatis gravida leo, ac rhoncus dui iaculis eu. Curabitur id fermentum magna. Fusce semper enim et sem luctus gravida. Ut pretium turpis et porttitor iaculis. Nulla feugiat justo et purus dignissim maximus. Mauris id mauris quam. Quisque et metus sed massa blandit sagittis. Ut sagittis sem eget nibh bibendum convallis. In scelerisque mi est, non viverra tortor cursus sed. Aenean varius sed ante id dictum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum tellus urna, laoreet quis odio in, auctor placerat metus.
 		</main>
     </div>
   );
--- personal-website/src/components/navbar.jsx
+++ personal-website/src/components/navbar.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class NavBar extends Component {
 	render() {
 		return (
-			<React.Fragment>
-				<header>
-					<nav className="navbar">
-						<ul>
-							<li>About</li>
-							<li>Projects</li>
-							<li>Contact</li>
-						</ul>
-					</nav>
-				</header>
-			</React.Fragment>
+			<div className="navbar">
+				<nav>
+					<ul>
+						<li><a class="about" href="#About">About</a></li>
+						<li><a class="projects" href="#Projects">Projects</a></li>
+						<li><a class="contact" href="#Contact">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
 		);
 	}
 }
