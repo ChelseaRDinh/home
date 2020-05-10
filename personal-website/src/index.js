c3d4b2b073749c57a4fc74e6b9d312b163409a3c
chelseardinh <chelseadinh94@gmail.com>
2020-05-10 11:08:20 -0700

    Add fonts

--- personal-website/src/index.js
+++ personal-website/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './fonts/YesevaOne-Regular.ttf'
+import './fonts/MerriweatherSans-Regular.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
