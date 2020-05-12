61903fd6d7900adba1a847edd1252fc15fac2773
chelseardinh <chelseadinh94@gmail.com>
2020-05-12 10:30:27 -0700

    Add styling to contact form

--- personal-website/src/components/contact.jsx
+++ personal-website/src/components/contact.jsx
@@ -51,14 +51,14 @@ class Contact extends Component {
 		return (
 			<div className="contact">
 				<form className="contact-form" onSubmit={this.handleSubmit}>
-					<label className="message" htmlFor="message-input">Your Message</label>
-					<textarea onChange={e => this.setState({ message: e.target.value})} name="message" className="message-input" type="text" placeholder="Please write your message here" value={this.state.message} required/>
-
-					<label className="message-name" htmlFor="message-name">Your Name</label>
+				<label className="message-name" htmlFor="message-name">Name</label>
 					<input onChange={e => this.setState({ name: e.target.value})} name="name" className="message-name" type="text" placeholder="Your Name" value={this.state.name}/>
 
-					<label className="message-email" htmlFor="message-email">Your Email</label>
+					<label className="message-email" htmlFor="message-email">Email</label>
 					<input onChange={(e) => this.setState({ email: e.target.value})} name="email" className="message-email" type="email" placeholder="your@email.com" required value={this.state.email} />
+
+					<label className="message" htmlFor="message-input">Your Message</label>
+					<textarea onChange={e => this.setState({ message: e.target.value})} name="message" className="message-input" type="text" placeholder="Send me a message!" value={this.state.message} required/>
 
 					<div className="button--container">
 						<button type="submit" className="button button-primary">{ this.state.buttonText }</button>
