3a90d9943be72445762ef95b9b0b92a7c0bffc68
chelseardinh <chelseadinh94@gmail.com>
2020-05-14 11:37:29 -0700

    Update api uri

--- website/src/components/contact.jsx
+++ website/src/components/contact.jsx
@@ -31,7 +31,7 @@ class Contact extends Component {
 		};
 
 		//When testing locally uncomment below:
-		axios.post('https://localhost:4444', data)
+		axios.post('https://react-website-node-api-6mzswoq33.now.sh/api/v1', data)
 		.then( res => {
 			this.setState({ sent: true }, this.resetForm());
 		}).catch( () => {
