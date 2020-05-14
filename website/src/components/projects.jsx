20dba6621473114de0ed2eaf0f3c4d483337edfe
chelseardinh <chelseadinh94@gmail.com>
2020-05-14 09:33:40 -0700

    Update folder names

---
+++ website/src/components/projects.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="projects slide-up">
+				<hr className="content-divider" />
+				Projects page. Coming soon!
+				<hr className="content-divider" />
+			</div>
+		);
+	}
+}
+
+export default Projects;
