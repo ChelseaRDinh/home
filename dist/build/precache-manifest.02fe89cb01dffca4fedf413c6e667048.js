93cd5f19d58d29d4c91aa3879dd4fff0607a4420
chelseardinh <chelseadinh94@gmail.com>
2020-05-14 10:43:38 -0700

    Move build folder

---
+++ dist/build/precache-manifest.02fe89cb01dffca4fedf413c6e667048.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0143db15dcd225eb42b0ab13ab8b44d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "9ed93e6ddf9a8314ab76",
+    "url": "/home/static/css/main.5f0c3031.chunk.css"
+  },
+  {
+    "revision": "5052e7b734fa25f8449e",
+    "url": "/home/static/js/2.db5dd7c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.db5dd7c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed93e6ddf9a8314ab76",
+    "url": "/home/static/js/main.8df062a0.chunk.js"
+  },
+  {
+    "revision": "dc54be341291695f976d",
+    "url": "/home/static/js/runtime-main.8d18b7ad.js"
+  },
+  {
+    "revision": "34a6a92decdadd0db2934b237988579e",
+    "url": "/home/static/media/resume_2020.34a6a92d.pdf"
+  }
+]);
--- dist/build/service-worker.js
+++ dist/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37aaab7b821bcdee81b6ed339eb7a0e5.js"
+  "/home/precache-manifest.02fe89cb01dffca4fedf413c6e667048.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
